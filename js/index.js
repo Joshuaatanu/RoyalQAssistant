4e6663df088ca599b097d9557285edf350edd975
Emeje Atanu <atanu.joshua@gmail.com>
2022-11-26 06:39:19 +0100

    my second commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// main javascript
+    let card = document.getElementById("card");
+    card.onmouseover = () =>{
+        card.style.backgroundColor = "#07bebe";
+        card.style.color= "#fff";
+    }
+
+    card.onmouseout = () =>{
+        card.style.backgroundColor = "white";
+        card.style.color= "black";
+    }
+
